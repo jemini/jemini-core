effb2142d2c57ece110df850821e4daf35664c91
David Koontz <david@koontzfamily.org>
2008-05-31 02:29:24 -0700

    Created a main Gemini class that will eventually serve as the start point of the app.  Messing around with getting the Slick input system working.  Got events from Slick which I'm pumping into the message queue.  Need a real input manager class that can map slick input events into actual game events to be consumed by game object behaviors.

--- src/gemini.rb
+++ src/gemini.rb
@@ -1,8 +1,49 @@
-class Gemini
-  def initialize
-    @input_manager = InputManager.new(screen_height)
-    @message_queue = MessageQueue.new
+include_class 'org.newdawn.slick.BasicGame'
+include_class 'org.newdawn.slick.AppGameContainer'
+include_class 'org.newdawn.slick.Input'
+include_class 'org.newdawn.slick.InputListener'
+
+require 'message_queue'
+
+class GeminiInputListener
+  include InputListener
+  
+  def isAcceptingInput
+    true
   end
   
+  def method_missing(method, *args)
+    return if method == :inputEnded
+    MessageQueue.instance.post_message(:slick_input, [method, args])
+  end
+end
+
+class Gemini < BasicGame
+  def initialize(screen_title, screen_width, screen_height, fullscreen=false)
+    super(screen_title)
+    @screen_width, @screen_height = screen_width, screen_height
+    app = AppGameContainer.new(self, screen_width, screen_height, fullscreen)
+    app.start
+  end
+  
+  def init(container)
+    @raw_input = container.input
+    @raw_input.add_listener GeminiInputListener.new
     
+    # Replace with an input manager class that consumes raw slick_input events
+    # and output relevant events based on some configurable input mapping
+    # something like KEY_A => :left, KEY_W => :up, etc.
+    MessageQueue.instance.add_listener(:slick_input, self) do |type, message|
+      puts "#{message[0]}: #{message[1]}"
+    end
+    MessageQueue.instance.start_processing
+  end
+  
+  def update(container, delta)
+    @raw_input.poll(@screen_width, @screen_height)
+  end
+  
+  def render(container, graphics)
+    
+  end
 end
