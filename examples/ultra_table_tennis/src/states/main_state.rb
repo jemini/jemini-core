be80da0ac6c79b6c84c9b44ac0a7caf1a87eb2bc
Logan Barnett <logustus@gmail.com>
2008-08-16 03:40:28 -0700

    Removed tag manager (part of Gemini Proper now). Using create_game_object to create and add game objects.

--- examples/ultra_table_tennis/src/states/main_state.rb
+++ examples/ultra_table_tennis/src/states/main_state.rb
@@ -3,26 +3,23 @@ require 'ball'
 
 class MainState < Gemini::BaseState 
   def load
-    set_manager(:tag, TagManager.new(self))
+    set_manager(:tag, create_game_object(:TagManager))
     load_keymap :MainGameKeymap
     
     3.times do
-      ball = Ball.new [rand(11)-5, rand(11)-5]
+      ball = create_game_object :Ball, [rand(11)-5, rand(11)-5]
       ball.move(rand(640 -ball.width), rand(480 - ball.height))
-      add_game_object ball
     end
     
-    add_game_object Wall.new(0, -10, 640, 10)
-    add_game_object Wall.new(640, 0, 10, 480)
-    add_game_object Wall.new(-10, 0, 10, 480)
-    add_game_object Wall.new(0, 480, 640, 10)
+    create_game_object :Wall, 0, -10, 640, 10
+    create_game_object :Wall, 640, 0, 10, 480
+    create_game_object :Wall, -10, 0, 10, 480
+    create_game_object :Wall, 0, 480, 640, 10
     
-    paddle = Paddle.new(1)
+    paddle = create_game_object :Paddle, 1
     paddle.move(100, 100)
-    add_game_object paddle
     
-    paddle = Paddle.new(2)
+    paddle = create_game_object :Paddle, 2
     paddle.move(500, 300)
-    add_game_object paddle
   end
 end
