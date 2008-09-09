03a62eee9d1b20db1b2d9420e53ada28f6c4fbab
Logan Barnett <logustus@gmail.com>
2008-09-09 00:16:03 -0700

    Updated some documentation. Added polygon support to DebugTangible.

--- src/behaviors/debug_tangible.rb
+++ src/behaviors/debug_tangible.rb
@@ -10,22 +10,16 @@ class DebugTangible < Drawable
   end
   
   def draw(graphics)
+    #TODO: Support joints and composite bodies(?)
     body = @target.instance_variable_get(:@behaviors)[:Tangible].instance_variable_get(:@body)
     physics_shape = body.shape
-    graphics_shape = if physics_shape.kind_of? Java::net::phys2d::raw::shapes::Box
-                       #Java::org.newdawn.slick.geom.Rectangle.new(body.position.x, body.position.y, physics_shape.size.x, physics_shape.size.y)
+    graphics_shape = if physics_shape.kind_of?(Java::net::phys2d::raw::shapes::Box) || physics_shape.kind_of?(Java::net.phys2d.raw.shapes.Polygon)
                        Java::org.newdawn.slick.geom.Polygon.new(physics_shape.get_points(body.position, body.rotation).map{|point| [point.x, point.y]}.flatten.to_java(:float))
                      elsif physics_shape.kind_of? Java::net.phys2d.raw.shapes.Circle
                        Java::org.newdawn.slick.geom.Circle.new(body.position.x, body.position.y, physics_shape.radius)
-                     elsif physics_shape.kind_of? Java::net.phys2d.raw.shapes.Polygon
-                       puts "creating a polygon"
-#                       Java::org.newdawn.slick.geom.Polygon.new(points.map{|point| [point.x, point.y]}.flatten.to_java(:float))
                      else
-                       puts "could not match any type"
+                       raise "DebugTangible does not know how to draw the shape #{physics_shape.class}"
                      end
-    #TODO: Get the points of rotation and draw it                 
-    #graphics_shape = graphics_shape.transform Java::org::newdawn::slick::geom::Transform.create_rotate_transform(body.rotation * Tangible::RADIANS_TO_DEGREES_MULTIPLIER, body)
-
     graphics.draw(graphics_shape)
   end
 end
