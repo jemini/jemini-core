38eaf8037ae33c3830534983239d5fff083a4e38
Logan Barnett <logustus@gmail.com>
2008-10-25 20:17:17 -0700

    Forgot to add RotatesToPoint.

---
+++ src/behaviors/rotates_to_point.rb
@@ -0,0 +1,18 @@
+# This behavior rotates to face the tracking point or game object
+class RotatesToPoint < Gemini::Behavior
+  #TODO: Add offset rotation
+  depends_on :Tangible
+  attr_accessor :rotation_target
+  alias_method :set_rotation_target, :rotation_target=
+  declared_methods :set_rotation_target, :rotation_target=, :rotation_target
+  
+  def load
+    @target.on_update do
+      @target.set_rotation rotation_to_face_target if @rotation_target
+    end
+  end
+  
+  def rotation_to_face_target
+    diff_angle = Gemini::Math.radians_to_degrees Math.atan2(@target.y - @rotation_target.y, @target.x - @rotation_target.x)
+  end
+end
