0dde3572b682bcc8170f4eed0cfa3a558db4ae70
Logan Barnett <logustus@gmail.com>
2009-05-15 13:14:17 -0700

    Trying to use new rake features to allow building of examples.

--- examples/life-tank/build_configuration.rb
+++ examples/life-tank/build_configuration.rb
@@ -1,11 +1,16 @@
+GEMINI_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+
 configuration do |c|
   c.project_name = 'ChangeMe'
   c.output_dir = 'package'
   c.main_ruby_file = 'main'
   c.main_java_file = 'org.rubyforge.rawr.Main'
-
+  c.copy_to_build = [{:from => File.join(GEMINI_DIR, 'lib'), :to => 'lib/java'},
+                     {:from => File.join(GEMINI_DIR, 'src'), :to => '.'}
+                    ]
   # Compile all Ruby and Java files recursively
   # Copy all other files taking into account exclusion filter
+#  c.source_dirs = ['src', 'lib/ruby', {:from => File.join(GEMINI_DIR, 'src'), :to => '.'}]
   c.source_dirs = ['src', 'lib/ruby']
   c.source_exclude_filter = []
 
@@ -15,9 +20,10 @@ configuration do |c|
   #c.jruby_jar = 'lib/java/jruby-complete.jar'
   c.compile_ruby_files = true
   #c.java_lib_files = []  
+#  c.java_lib_dirs = [:from => File.join(GEMINI_DIR, 'lib'), :to => 'lib/java']
   c.java_lib_dirs = ['lib/java']
   #c.files_to_copy = []
-
+  c.java_library_path = File.join 'lib', 'native_files'
   c.target_jvm_version = 1.5
   #c.jars[:data] = { :directory => 'data/images', :location_in_jar => 'images', :exclude => /bak/}
   #c.jvm_arguments = ""
