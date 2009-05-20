caeecc7ed6479289dbe4af480aed666751bd227e
Logan Barnett <logustus@gmail.com>
2009-05-20 08:35:28 -0700

    Added some more constants so we see less magic numbers. Ground uses fewer points. Tanks don't fall through ground anymore because Physics manager chops up large deltas to prevent aliasing problems with movement.

--- examples/life-tank/src/main.rb
+++ examples/life-tank/src/main.rb
@@ -1,3 +1,11 @@
+$profiling = false #change this to true to start the profiler
+if $profiling
+  require 'profile'
+  Java::java::lang::Runtime.runtime.add_shutdown_hook(Java::java::lang::Thread.new do
+    Profiler__::print_profile(STDERR) if $profiling
+  end)
+end
+
 $LOAD_PATH.clear
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../../../src')
