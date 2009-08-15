dc5bdb5a0f3677f55e8c6392ed83f7d1c22f3f1c
Logan Barnett <logustus@gmail.com>
2009-08-14 19:43:01 -0700

    Fixed GridBound's snap_to_grid. Vector's inspect should give at least x/y now. GridBound's adjacent_grid methods now support diaganol cardinal directions. Took out Main from gemini.rb and renamed it Game. Instances methods on Game allow for a game instance to be used in other apps (such as the Features app). Game needs tests. Skeleton needs fixing.

---
+++ src/game.rb
@@ -0,0 +1,103 @@
+module Gemini
+  # TODO: Ensure generation uses Game instead of Main
+  # TODO: Move class methods start_app and create_canvas to instance equivalents
+  # TODO: Ensure generation uses new instance version of methods
+  class Game < Java::org::newdawn::slick::BasicGame
+    include_class 'org.newdawn.slick.AppGameContainer'
+    attr_accessor :screen_size, :fullscreen, :initial_state
+
+    def initialize(options={})
+      super(options[:screen_title] || 'Jemini Game')
+      @initial_state = options[:initial_state] || :MainState
+      @screen_size   = options[:screen_size] || Vector.new(800, 600)
+      @fullscreen    = options[:fullscreen]
+      @always_render = options[:always_render]
+      @fresh_state   = true
+    end
+
+    def init(container)
+      @container = container
+      @container.always_render = @always_render
+      BaseState.active_state = load_state(@initial_state)
+      BaseState.active_state.load
+    end
+
+    # depcrecated
+    def screen_width
+      screen_size.x
+    end
+
+    # depcrecated
+    def screen_height
+      screen_size.y
+    end
+
+    def app
+      @app ||= create_app
+    end
+
+    def canvas
+      @canvas ||= create_canvas
+    end
+
+    def update(container, delta)
+      #don't tell the new state that it now has to update load time worth of a delta
+      if @fresh_state
+        delta = 0
+        @fresh_state = false
+      end
+      # Workaround for image loading with Slick.
+      # Must be done in game init or game loop (instead of immediately in the event).
+      if @queued_state
+        @queued_state.load(*@state_args)
+        BaseState.active_state = @queued_state
+        @queued_state = nil
+        @fresh_state = true
+        return
+      end
+      BaseState.active_state.manager(:input).poll(screen_size.x, screen_size.y, delta)
+      BaseState.active_state.manager(:message_queue).process_messages(delta)
+      BaseState.active_state.manager(:update).update(delta)
+      BaseState.active_state.manager(:game_object).__process_pending_game_objects
+    end
+
+    def render(container, graphics)
+      BaseState.active_state.manager(:render).render(graphics)
+    end
+
+    def load_state(state_name, args = [])
+      require "states/#{state_name.underscore}" unless Object.const_defined? state_name.camelize
+      @state_args = args
+      state_name.camelize.constantize.new @container, self
+    end
+
+    def switch_state(state_name)
+      queue_state load_state(state_name)
+    end
+
+    def queue_state(state)
+      @queued_state = state
+    end
+
+  private
+    def create_app
+      @app = AppGameContainer.new(self, screen_size.x, screen_size.y, fullscreen)
+      @app.vsync = false
+      @app.maximum_logic_update_interval = 60
+      @app.smooth_deltas = false #true
+      #main.container = container
+      @app.start
+    end
+
+    def create_canvas
+      @canvas = Java::org::newdawn::slick::CanvasGameContainer.new(self)
+      @canvas.set_size(screen_size.x, screen_size.y)
+      container = @canvas.container
+      container.vsync = false
+      container.maximum_logic_update_interval = 60
+      #container.start
+      @canvas
+    end
+
+  end
+end
