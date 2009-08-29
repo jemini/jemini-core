f04ddc0203bb7beddbe3556a0c1d88a7c06f0b64
Logan Barnett <logustus@gmail.com>
2009-08-29 11:49:10 -0700

    Updates uses updated means of registering listeners.

--- src/behaviors/updates.rb
+++ src/behaviors/updates.rb
@@ -1,9 +1,6 @@
 #Makes an object receive events for changes in state.
 class Updates < Gemini::Behavior
-  
-  def load
-    @target.enable_listeners_for :update
-  end
+  listen_for :update
   
   def update(delta)
     @target.notify :update, delta
