46155847ec106d4be2c4d62baca5b155c3f07da9
Jay McGavren <jay@mcgavren.com>
2009-09-12 01:39:06 -0700

    Updated tag manager spec.

---
+++ test/unit/managers/tag_manager_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+# require File.join(File.dirname(__FILE__), 'shared_spec')
+require 'managers/tag_manager'
+
+describe 'TagManager' do
+
+  # it_should_behave_like "a manager"
+
+  before :each do
+    @state = Jemini::BaseState.new(mock('Container', :null_object => true), mock('Game', :null_object => true))
+    @tag_manager = TagManager.new(@state)
+    @state.send(:set_manager, :tag, @tag_manager)
+  end
+  
+  it "tracks tags that are added to objects" do
+    object = @state.create :GameObject, :Taggable
+    object.add_tag :foo
+    @state.manager(:tag).find_by_tag(:foo).should include(object)
+  end
+  
+end
