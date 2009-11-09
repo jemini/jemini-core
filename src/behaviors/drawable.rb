4b0d917111bba28d003cc8f39975f8f412344d93
Logan Barnett <logustus@gmail.com>
2009-11-08 21:59:25 -0700

    Removed no longer needed behaviors

--- src/behaviors/drawable.rb
+++
@@ -1,7 +0,0 @@
-#Makes an object draw itself to the screen.
-class Drawable < Jemini::Behavior
-  depends_on_kind_of :Spatial
-  wrap_with_callbacks :draw
-
-  def draw(graphics); end
-end
--- src/behaviors/world_collidable.rb
+++
@@ -1,9 +0,0 @@
-#Makes an object generate an event if it collides with another.
-class WorldCollidable < Jemini::Behavior
-  depends_on :BoundingBoxCollidable
-  depends_on_kind_of :CollisionPoolAlgorithm
-  
-  def world_collision_check
-    get_collision_candidates.each {|candidate| collision_check candidate}
-  end
-end
