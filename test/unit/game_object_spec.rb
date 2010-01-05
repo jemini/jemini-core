f62f6aa388af36acef2f4204d2549cbd70c91c1e
Logan Barnett <logustus@gmail.com>
2010-01-05 13:30:39 -0700

    Added game object specs.

---
+++ test/unit/game_object_spec.rb
@@ -0,0 +1,62 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe "Jemini::GameObject" do
+  it_should_behave_like "initial mock state"
+
+  before :each do
+    @game_object = Jemini::GameObject.new(@state)
+  end
+
+  it "removes listeners after invoking on_after_remove callbacks" do
+    pending
+    invoked = false
+    @game_object.on_after_remove do
+      invoked = true
+    end
+
+    @game_object.__destroy
+    invoked.should be_true
+
+    invoked = false
+    @game_object.__destroy # destroying again proves invoked should not be called because handlers are removed
+    invoked.should be_false
+  end
+
+  it "has a before_remove event" do
+    invoked = false
+    @game_object.on_before_remove do
+      invoked = true
+    end
+
+    @game_object.__destroy
+
+    invoked.should be_true
+  end
+
+  it "has an after_remove event" do
+    invoked = false
+    @game_object.on_after_remove do
+      invoked = true
+    end
+
+    @game_object.__destroy
+
+    invoked.should be_true
+  end
+
+  it "invokes the before_remove event before the after_remove event" do
+    status = nil
+    @game_object.on_before_remove do
+      status = :before
+    end
+
+    @game_object.on_after_remove do
+      status = :after
+    end
+
+    @game_object.__destroy
+    status.should == :after
+  end
+
+  
+end
