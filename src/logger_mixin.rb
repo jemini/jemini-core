e8b72b18690262dfe1be7372305e4b6fa5b6225d
Logan Barnett <logustus@gmail.com>
2010-01-06 06:31:35 -0700

    Fixed logger to respect env variables.

--- src/logger_mixin.rb
+++ src/logger_mixin.rb
@@ -3,9 +3,11 @@ require 'logger'
 module LoggerMixin
 
   #Get the Logger object (see the Ruby standard library documentation).
-  #The log level is set to match the $LOG_LEVEL environment variable, or Logger::ERROR by default.
+  #The log level is set to match the LOG_LEVEL environment variable, or Logger::ERROR by default.
   def log
-    @log ||= Logger.new(STDOUT, ENV['LOG_LEVEL'] || Logger::ERROR)
+    @log ||= Logger.new(STDOUT)
+    @log.level = (ENV['LOG_LEVEL'] and "Logger::#{ENV['LOG_LEVEL']}".constantize) || Logger::ERROR
+    @log
   end
   
 end
